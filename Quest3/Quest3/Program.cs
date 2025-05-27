545379de9feb9458113f08d9757581b559d576d7
LostMyNickName <lukdiman233@gmail.com>
2025-05-27 14:50:30 +0700

    Quest 3 beta

---
+++ Quest3/Quest3/Program.cs
@@ -0,0 +1,265 @@
+﻿using System;
+
+namespace Quest3
+{
+
+    class SquareMatrix
+    {
+
+        public int ColsAndRows { get; set; }
+        public int[,] matrix;
+
+        public SquareMatrix(int Cols)
+        {
+            matrix = new int[Cols, Cols];
+            Random rand = new Random();
+
+            for (int i = 0; i < Cols; i++)
+            {
+                for (int j = 0; j < Cols; j++)
+                {
+                    matrix[i, j] = rand.Next(0, 100);
+                }
+            }
+        }
+
+        public void getmatrix()
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    Console.Write($"{matrix[i, j]}\t");
+                }
+                Console.Write("\n");
+            }
+        }
+
+        public static SquareMatrix operator +(SquareMatrix matrix1, SquareMatrix matrix2)
+        {
+            SquareMatrix MatrixResult = new SquareMatrix(matrix1.matrix.GetLength(0));
+
+            if (matrix1.matrix.GetLength(0) != matrix2.matrix.GetLength(0))
+            {
+                MatrixResult.matrix = null;
+                return MatrixResult;
+            }
+            else
+            {
+
+                for (int i = 0; i < matrix1.matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix1.matrix.GetLength(0); j++)
+                    {
+                        MatrixResult.matrix[i, j] = matrix1.matrix[i, j] + matrix2.matrix[i, j];
+                    }
+                }
+
+                return MatrixResult;
+            }
+        }
+
+        public static SquareMatrix operator -(SquareMatrix matrix1, SquareMatrix matrix2)
+        {
+            SquareMatrix MatrixResult = new SquareMatrix(matrix1.matrix.GetLength(0));
+            if (matrix1.matrix.GetLength(0) != matrix2.matrix.GetLength(0))
+            {
+                MatrixResult.matrix = null;
+                return MatrixResult;
+            }
+            else
+            {
+
+
+                for (int i = 0; i < matrix1.matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix1.matrix.GetLength(0); j++)
+                    {
+                        MatrixResult.matrix[i, j] = matrix1.matrix[i, j] - matrix2.matrix[i, j];
+                    }
+                }
+
+                return MatrixResult;
+            }
+        }
+
+        public static SquareMatrix operator *(SquareMatrix matrix1, SquareMatrix matrix2)
+        {
+            SquareMatrix MatrixResult = new SquareMatrix(matrix1.matrix.GetLength(0));
+            int[,] result = new int[matrix1.matrix.GetLength(0), matrix1.matrix.GetLength(0)];
+
+            for (int i = 0; i < matrix1.matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix2.matrix.GetLength(1); j++)
+                {
+                    for (int k = 0; k < matrix2.matrix.GetLength(0); k++)
+                    {
+                        result[i, j] += matrix1.matrix[i, k] * matrix2.matrix[k, j];
+                    }
+                }
+            }
+
+            MatrixResult.matrix = result;
+            return MatrixResult;
+        }
+
+        public static bool operator ==(SquareMatrix matrix1, SquareMatrix matrix2)
+        {
+            for (int i = 0; i < matrix1.matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix1.matrix.GetLength(0); j++)
+                {
+                    if (matrix1.matrix[i, j] != matrix2.matrix[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public static bool operator !=(SquareMatrix matrix1, SquareMatrix matrix2)
+        {
+            for (int i = 0; i < matrix1.matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix1.matrix.GetLength(0); j++)
+                {
+                    if (matrix1.matrix[i, j] != matrix2.matrix[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        //public static bool operator <(SquareMatrix matrix1, SquareMatrix matrix2)
+        //{
+        //    //Из того, что я нагуглил, нет общепринятого понятия сравнивания матриц
+        //    return ЕГГОГ; 
+        //}
+        //public static bool operator >(SquareMatrix matrix1, SquareMatrix matrix2)
+        //{
+        //    //Из того, что я нагуглил, нет общепринятого понятия сравнивания матриц
+        //    return ЕГГОГ; 
+        //}
+
+        public static bool operator <=(SquareMatrix matrix1, SquareMatrix matrix2)
+        {
+            for (int i = 0; i < matrix1.matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix1.matrix.GetLength(0); j++)
+                {
+                    if (matrix1.matrix[i, j] != matrix2.matrix[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public static bool operator >=(SquareMatrix matrix1, SquareMatrix matrix2)
+        {
+            for (int i = 0; i < matrix1.matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix1.matrix.GetLength(0); j++)
+                {
+                    if (matrix1.matrix[i, j] != matrix2.matrix[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+    }
+
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int ColsAndRows = 0;
+            Console.Write("Введите количество строк и столбцов первой квадратной матрицы: ");
+            try
+            {
+                ColsAndRows = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Ну это ведь вообще не число");
+                return 0;
+            }
+            if (ColsAndRows <= 0)
+            {
+                Console.WriteLine("АХТУНГ АШИБКА");
+                return 0;
+            }
+
+            SquareMatrix Matrix1 = new SquareMatrix(ColsAndRows);
+            Matrix1.getmatrix();
+
+            Console.Write("Введите количество строк и столбцов второй квадратной матрицы: ");
+            ColsAndRows = Convert.ToInt32(Console.ReadLine());
+            if (ColsAndRows <= 0)
+            {
+                Console.WriteLine("АХТУНГ АШИБКА");
+                return 0;
+            }
+
+            SquareMatrix Matrix2 = new SquareMatrix(ColsAndRows);
+            Matrix2.getmatrix();
+
+            Console.WriteLine("Введите желаемое действие:\n" +
+                "1.Сложение матриц\n" +
+                "2.Вычитание матриц\n" +
+                "3.Умножение матриц\n" +
+                "4.Равны ли матрицы");
+
+            int choise = Convert.ToInt32(Console.ReadLine());
+
+            switch (choise)
+            {
+                case 1:
+                    Console.WriteLine("Сумма матриц: ");
+                    SquareMatrix MatrixSum = Matrix1 + Matrix2;
+                    if (MatrixSum.matrix != null)
+                        MatrixSum.getmatrix();
+                    else
+                        Console.WriteLine("Для вычисления суммы матриц длина строк и столбцов должны быть равны");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Разница матриц: ");
+                    SquareMatrix MatrixMinus = Matrix1 - Matrix2;
+                    if (MatrixMinus.matrix != null)
+                        MatrixMinus.getmatrix();
+                    else
+                        Console.WriteLine("Для вычисления разницы матриц длина строк и столбцов должны быть равны");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Умножение матриц: ");
+                    SquareMatrix MatrixMultiplication = Matrix1 * Matrix2;
+                    MatrixMultiplication.getmatrix();
+                    break;
+
+                case 4:
+                    if (Matrix1 == Matrix2)
+                    {
+                        Console.WriteLine("Матрицы равны");
+                    }
+                    if (Matrix1 != Matrix2)
+                    {
+                        Console.WriteLine("Матрицы НЕ равны");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("БИПБУП ОШИБКА!\nНет такого варианта");
+                    break;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Quest3/Quest3/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Quest3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Quest3")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("54908f11-9980-4344-a6e8-74e84c3a1785")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
